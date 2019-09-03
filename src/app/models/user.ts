d279243176765d9cfab7c339630c43824b5d3857
Amol-Dev <54835843+Amol-Dev@users.noreply.github.com>
2019-09-03 16:23:58 +0530

    Add files via upload

---
+++ src/app/models/user.ts
@@ -0,0 +1,20 @@
+export interface IUser {
+  userId: number;
+  userName: string;
+  userPassword: string;
+  userEmail: string;
+}
+
+export interface IEmployee {
+  id: number;
+  fName: string;
+  lName: string;
+  technology: string;
+  designation: string;
+  skills: string;
+  coreExpertise: string;
+  isCertification: boolean;
+  certificationName: string;
+  compnay: string;
+  about: string;
+}
